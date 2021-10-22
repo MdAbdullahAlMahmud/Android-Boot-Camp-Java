ad64892ac0f92384b6bc22bb53865e2a613c3b89
MdAbdullahAlMahmud <mmunna864@gmail.com>
2021-10-22 20:31:04 +0600

    class4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,86 +1,26 @@
 package com.company;
 
-
 public class Main {
 
     public static void main(String[] args) {
-       /* int a ;
-        a = 15;
-        float b;
-        double d;
-        String word = "djfksldjfksdjf";
-        boolean isOdd = true;
-        boolean isEven = false;
-        long l ;
-        char ch = 'a';
-        System.out.println(a);
-        int m = 10 , n=10 ;
-        if (m>n){
-            System.out.println("M is grater that N");
-        }else if(m==n){
-            System.out.println("M & N are same");
-        }
-        else {
-            System.out.println("N is grater that M");
-        }
 
-        switch (m){
-            case 1:
-                System.out.println("M = 1");
-            case 2 :
-                System.out.println("M = 1");
-            default:
-                System.out.println("M is out of range");
-        }*/
-       /* Scanner sc = new Scanner(System.in);
+        Mobile mobile = new Mobile("Samsung" , "12","31");
+       // mobile.showMessage();
+
+        Samsung samsung = new Samsung("12", "Samsung" , "22/10/2021");
+        samsung.audioCall();
+        samsung.videoCall();
+        samsung.variableRefreshRate();
+
+        Vivo vivo = new Vivo("11" , "Vevo", " 10/10/2021");
+        vivo.audioCall();
+        vivo.videoCall();
+        vivo.notificationPanel();
 
 
 
-        //float val = scanners.nextFloat();
-        String course = "Android Bootcamp ";
-       // System.out.println(course + val);
 
-        int m = 45;
 
-        int a = 10 ;
-        float val = (float) a;
-         //System.out.println("Value is " + val);
-
-        int y = 45;
-        String ys = y + " ";
-        Integer yt = 45;
-         System.out.println("Value is ");*/
-
-        String cpc = "Android Bootcamp 2.0";
-        //System.out.println(cpc);
-
-        //5
-        /*
-        1*1 = 1
-        1*2 = 2
-        .......
-        1*10 = 10
-
-        ----------------
-        2*1 = 2
-        2*2 = 4
-        ----------------
-        5*1 = 5
-        5*2 = 10
-
-         */
-
-        for (int a :oddNumberInRange(7)
-             ) {
-
-            System.out.println(a);
-        }
-
-    }
-
-    private static int[] oddNumberInRange(int range){
-        int arr[] = {1,3,5,7,9,11};
-        return arr;
     }
 
 }
